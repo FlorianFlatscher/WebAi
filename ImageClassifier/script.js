0d6abe57584780d087a6db9b39ac996b9c056c2e
BuildTools <unconfigured@null.spigotmc.org>
2020-01-08 11:09:17 +0100

    update

--- ImageClassifier/script.js
+++ ImageClassifier/script.js
@@ -6,8 +6,6 @@ const drop = document.querySelector("#drop");
 const icon = document.querySelector("#drop div");
 
 function setup() {
-
-
     svg.style.animation = "svg-grow-ring 1000ms infinite linear";
 }
 
@@ -17,7 +15,7 @@ function draw() {
 }
 
 function preload() {
-     classifier = ml5.imageClassifier('MobileNet', () => {
+    classifier = ml5.imageClassifier('MobileNet', () => {
         svg.style.display = "none";
         svg.style.animation = "none";
 
@@ -29,15 +27,15 @@ function preload() {
         dropzone.drop(gotFile, unhighlight);
 
         function gotFile(file) {
-
             if (img) {
-                img.remove();
+                drop.innerHTML = "";
             }
 
             img = createImg(file.data, "Dropped Image", "", () => {
                 classifier.classify(img, gotResult);
             });
         }
+
         function highlight() {
             dropzone.style('background-color', 'lightgrey');
         }
@@ -49,16 +47,18 @@ function preload() {
 }
 
 function gotResult(error, results) {
-    //Image
+
+//Image
+    drop.style.display = "grid";
+
     img.parent(drop);
     img.elt.classList.add("imageToC");
     icon.style.display = "none";
-
     // Display error in the console
     if (error) {
         console.error(error);
     }
     // The results are in an array ordered by confidence.
     console.log(results);
-
+    drop.innerHTML = drop.innerHTML + "<div id='classify'><p>" + results[0].label + ": %" + 100 * results[0].confidence + "</p><p>" + results[1].label + ": %" + 100 * results[1].confidence + "</p><p>" + results[2].label + ": %" + 100 * results[2].confidence + "</p></div>";
 }
